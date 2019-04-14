eba397c2e4f158c3f94344954c2745c67657c929
Jay Burbyga <jayburbyga@gmail.com>
2019-04-14 16:33:38 -0400

    last update before deploy

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0a8859df205648bab3f685ab2a03432.js"
+  "/precache-manifest.2b11222e4310d0f86b032271f68e767b.js"
 );
 
 workbox.clientsClaim();
