fbc6b1d70d09065fe85165071c2341ce9f1bcb6e
Jaybur1 <jayburbyga@gmail.com>
2020-04-10 11:46:45 -0400

    small fixes

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -1,34 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div className="about-page">
-    <h1 className="quote center">"Try to be better then tommorow self!"</h1>
-    <div className="about-content">
-      <p>
-        <b className="about-title" style={{ fontSize: '20px' }}>Who am I:</b><br />
-        Hi! My name is Evgeniy Burbyga ,
-        but you can call me Jay (my wife and mom calling me that).
-        I am 28 years old, was born in the Ukraine , moved to Israel with my family at the age of 6 ,
-        and then moved to Canada with my wife in 2017. I have a cat named Muffin and I play drums since 2004.
-<br />
-        <br />
-        <b className="about-title" style={{ fontSize: '20px' }}> Early life: </b><br />
-        Before I entered the big web development world ,
-        I was a professional truck driver, and web development was a side hobby that I really enjoyed.
-        I was teaching my self to code with FreeCodeCamp and Odin's Project while working 12hr/6day shifts(yeah, no life).
-        But I really enjoyed that I needed to use my brain a lot, and that I actually can build stuff that I can show or even just explain.
-        With that, Somewhere along the way this hobby became a passion, and the fillings and thinking, a way of life .
-        Then I realized that learning and developing my self is the thing that keeps my gears going.
-<br />
-        <br />
-        <b className="about-title" style={{ fontSize: '20px' }}>What I am up to now :</b><br />
-
-        As the quote on top of this page says, I am trying to be better then tomorrow me , I'm constantly learning and self developing.<br />
-        I'm always open for new challenging, hard and interesting projects! so if you have them, count me in !!! </p>
+      <h1 className="quote center">"Try to be better then tommorow self!"</h1>
+      <div className="about-content">
+        <p>
+          <b className="about-title" style={{ fontSize: "20px" }}>
+            Who am I:
+          </b>
+          <br />
+          Hi! My name is Evgeniy Burbyga , but you can call me Jay (my wife and
+          mom calling me that). I am 28 years old, was born in the Ukraine ,
+          moved to Israel with my family at the age of 6 , and then moved to
+          Canada with my wife in 2017. I have a cat named Muffin and I play
+          drums since 2004.
+          <br />
+          <br />
+          <b className="about-title" style={{ fontSize: "20px" }}>
+            {" "}
+            Early life:{" "}
+          </b>
+          <br />
+          My passion for coding started about 3 years ago. As any passion or
+          hobby in my life, the passion for coding rose out of the satisfaction
+          of getting various problems solved, creating something new and
+          figuring out the “how to” for any challenge that I encounter.
+          Researching and learning how to overcome a road block excites me with
+          anticipation of acquiring new knowledge. Learning to code helped me
+          realize that most of my interests were driven by and revolved around
+          the same base concept of my passion: whether it was to build something
+          visual and operational from construction assembly toys, or forming a
+          music band and writing music, or disassembling and trying to fix
+          things (like an old phone or a laptop) before discarding them, or
+          experimenting with raspberry pi mini computer. My passion became my
+          way of life. I was learning to code every bit of my free time, even
+          when working 12h/6d a week shifts. However, I felt it was not enough.
+          Fortunately, I got a chance to work on a few small projects with my
+          cousin, who was a senior full stack developer at that time. I saw this
+          as an opportunity to test my passion for coding and whether or not I
+          will get as much satisfaction when coding for someone with strict
+          demands as I was getting when I was coding for my own enjoyment. This
+          experience not only proved my passion for coding but also gave the
+          meaning to it. After moving to Ottawa, I fed my interest in coding
+          through regularly going to almost every available coding meetup or
+          workshop. Meeting and talking to new people from the industry gave me
+          the feeling that it was time to transform my passion into a
+          profession.
+          <br />
+          <br />
+          <b className="about-title" style={{ fontSize: "20px" }}>
+            What I am up to now :
+          </b>
+          <br />
+          As the quote on top of this page says, I am trying to be better then
+          tomorrow me , I'm constantly learning and self developing.
+          <br />
+          I'm always open for new challenging, hard and interesting projects! so
+          if you have them, count me in !!!{" "}
+        </p>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
